d89d69db2a868de26e4859e6eff5809b3baea91c
arslonga83 <jlwood83@gmail.com>
2022-12-31 22:02:33 -0800

    render colors from placeholder fetch

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-fetch('https://www.thecolorapi.com/scheme?hex=00ff00&mode=triad')
+const colorsGrid = document.getElementById('colors-grid')
+
+fetch('https://www.thecolorapi.com/scheme?hex=00ff00&mode=monochrome')
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    let gridHtml = ''
+    data.colors.map((item) => {
+      gridHtml += `
+      <div class="color" style="background: ${item.hex.value}">
+      <h4>${item.hex.value}</h4>
+      </div>
+      `
+    })
+    colorsGrid.innerHTML = gridHtml;
+  })
